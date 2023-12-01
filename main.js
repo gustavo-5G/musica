686714407130d3f602cdfc7a3b5f1f4d78ea4e37
gustavo-5G <131722213+gustavo-5G@users.noreply.github.com>
2023-12-01 19:32:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 som = ""
-leftWristX  = 0
-leftWristY  = 0
+scoreLeftWristX = 0
+scoreLeftWristY = 0
+leftWristX = 0
+leftWristY = 0
 rightWristX = 0
 rightWristY = 0
 
@@ -14,11 +16,28 @@ function setup() {
     canvas.center()
     video = createCapture(VIDEO)
     video.hide()
-    poseNet = ml5.poseNet(video,modelLoaded)
-    
+    poseNet = ml5.poseNet(video, modelLoaded)
+
 }
 function draw() {
     image(video, 0, 0, 600, 500)
+    fill("#FF0000")
+    stroke("FF0000")
+    circle(leftWristX, leftWristY, 20)
+    isNumber = Number(leftWristX)
+    remove = floor(isNumber)
+    volume = remove / 500
+    document.getElementById("volume").innerHTML = "volume e igual " + volume
+    som.setVolume(volume)
+
+    if (scoreLeftWristX > 0.2) {
+        circle(leftWristX, leftWristY, 20)
+        isNumber = Number(leftWristX)
+        remove = floor(isNumber)
+        volume = remove / 500
+        document.getElementById("volume").innerHTML = "volume e igual " + volume
+        som.setVolume(volume)
+    }
 }
 function play() {
     som.play()
@@ -27,12 +46,14 @@ function play() {
 }
 
 
-function modelLoaded(){
-console.log("poseNet inicializado")
+function modelLoaded() {
+    console.log("poseNet inicializado")
 }
 function gotResults(results) {
     if (results.lenght > 0) {
         console.log(results)
+        scoreLeftWristX = results[0].pose.keypoints[9].score
+        console.log(scoreLeftWristX)
 
         leftWristX = results[0].pose.leftWrist.x
         leftWristY = results[0].pose.leftWrist.y
